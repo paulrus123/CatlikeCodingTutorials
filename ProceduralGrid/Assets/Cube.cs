4fb2aa7eeafbfcebc95b00651e23e6b648739941
Paul Sorenson <psoren@umich.edu>
2019-07-29 17:52:05 -0400

    Initial projects commit

---
+++ ProceduralGrid/Assets/Cube.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Cube : MonoBehaviour {
+
+    public int xSize, ySize, zSize;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+
+    private void Awake () {
+        Generate();
+    }
+
+    private void Generate() {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Cube";
+
+        CreateVertices();
+        CreateTriangles();
+    }
+      
+   
+    void CreateVertices() { 
+        int cornerVertices = 8;
+        int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+        int faceVertices = (
+            (xSize - 1) * (ySize - 1) +
+            (xSize - 1) * (zSize - 1) +
+            (ySize - 1) * (zSize - 1)) * 2;
+        vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+        int v = 0;
+        for (int y = 0; y <= ySize; y++) {
+            for (int x = 0; x <= xSize; x++) {
+                vertices[v++] = new Vector3(x, y, 0);
+            }
+            for (int z = 1; z <= zSize; z++) {
+                vertices[v++] = new Vector3(xSize, y, z);
+            }
+            for (int x = xSize - 1; x >= 0; x--) {
+                vertices[v++] = new Vector3(x, y, zSize);
+            }
+            for (int z = zSize - 1; z > 0; z--) {
+                vertices[v++] = new Vector3(0, y, z);
+            }
+        }
+
+        for (int z = 1; z < zSize; z++) {
+            for (int x = 1; x < xSize; x++) {
+                vertices[v++] = new Vector3(x, ySize, z);
+            }
+        }
+        for (int z = 1; z < zSize; z++) {
+            for (int x = 1; x < xSize; x++) {
+                vertices[v++] = new Vector3(x, 0, z);
+            }
+        }
+        mesh.vertices = vertices;
+    }
+
+    private void CreateTriangles() { }
+
+
+    private void OnDrawGizmos () {
+        if (vertices == null) {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
